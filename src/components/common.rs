de76a06af4423dd95a8572a9806c2debe8741a2d
Samuel Boyden <sam@stboyden.com>
2021-11-29 00:50:15 +0000

    Fix regex issue 2

--- src/components/common.rs
+++ src/components/common.rs
@@ -1,17 +1,16 @@
-use regex::Regex;
+use regex::RegexBuilder;
 
 pub fn regex_filter(
     regex_string: &str,
     match_string: &str,
     case_sensitive: bool,
 ) -> bool {
-    let mut regex_string = regex_string.to_string();
+    let regex_string = regex_string.to_string();
 
-    if case_sensitive {
-        regex_string.push_str("/i");
-    }
-
-    if let Ok(regex) = Regex::new(&regex_string) {
+    if let Ok(regex) = RegexBuilder::new(&regex_string)
+        .case_insensitive(!case_sensitive)
+        .build()
+    {
         return regex.captures_iter(match_string).next().is_some();
     }
 
