4477de5c0c5d4c8b61a7432788af259518260111
Samuel Boyden <sam@stboyden.com>
2022-01-04 22:40:42 +0000

    Remove "Examine" button, now labels react to clicks (#15)

--- src/components/main_view_components/log_item.rs
+++ src/components/main_view_components/log_item.rs
@@ -1,48 +1,43 @@
 use chrono::{DateTime, Local};
 use codectrl_logger::Log;
-use egui::{Color32, RichText, Ui};
+use egui::{Color32, Label, RichText, Sense, Ui};
 
 type Received = (Log<String>, DateTime<Local>);
 
-fn draw_hover(ui: &mut Ui) {
-    ui.label("Click on the \"\u{1f50e} Examine\" button to examine a log");
-}
+fn draw_hover(ui: &mut Ui) { ui.label("Click to view log"); }
 
 pub fn draw_log_item(
     clicked_item: &mut Option<Received>,
     received @ (log, time): &Received,
     ui: &mut Ui,
 ) {
-    ui.horizontal(|ui| {
-        if let Some(clicked_item) = &clicked_item {
-            let _checked = ui
-                .radio(*received == *clicked_item, "")
-                .on_hover_ui_at_pointer(draw_hover);
-        } else {
-            let _checked = ui.radio(false, "").on_hover_ui_at_pointer(draw_hover);
-        }
+    let radio_response = ui
+        .horizontal(|ui| {
+            let response = if let Some(clicked_item) = &clicked_item {
+                ui.radio(*received == *clicked_item, "")
+            } else {
+                ui.radio(false, "")
+            };
 
-        // u1f50e = 🔎
-        if ui.button("Examine \u{1f50e}").clicked() {
-            *clicked_item = Some((*received).clone());
-        }
+            if !log.warnings.is_empty() {
+                ui.label(
+                    RichText::new(format!("\u{26a0} {}", log.warnings.len())) // u26a0 = ⚠
+                        .color(Color32::YELLOW),
+                )
+                .on_hover_ui_at_pointer(|ui| {
+                    ui.heading("Logger generated the following warning(s)");
 
-        if !log.warnings.is_empty() {
-            ui.label(
-                RichText::new(format!("\u{26a0} {}", log.warnings.len())) // u26a0 = ⚠
-                    .color(Color32::YELLOW),
-            )
-            .on_hover_ui_at_pointer(|ui| {
-                ui.heading("Logger generated the following warning(s)");
+                    ui.label("");
 
-                ui.label("");
+                    for (index, warning) in log.warnings.iter().enumerate() {
+                        ui.label(format!("{}. {}", index + 1, warning));
+                    }
+                });
+            }
 
-                for (index, warning) in log.warnings.iter().enumerate() {
-                    ui.label(format!("{}. {}", index + 1, warning));
-                }
-            });
-        }
-    });
+            response
+        })
+        .inner;
 
     let mut message = log.message.replace("\"", "");
 
@@ -51,12 +46,33 @@ pub fn draw_log_item(
         message.push_str("...");
     }
 
-    ui.label(message).on_hover_ui_at_pointer(draw_hover);
-    ui.label(&log.address).on_hover_ui_at_pointer(draw_hover);
-    ui.label(&log.file_name).on_hover_ui_at_pointer(draw_hover);
-    ui.label(format!("{}", log.line_number))
-        .on_hover_ui_at_pointer(draw_hover);
-    ui.label(time.format("%F %X").to_string())
-        .on_hover_ui_at_pointer(draw_hover);
+    let labels = vec![
+        Label::new(message),
+        Label::new(&log.address),
+        Label::new(&log.file_name),
+        Label::new(format!("{}", log.line_number)),
+        Label::new(time.format("%F %X").to_string()),
+    ];
+
+    let mut responses = vec![];
+
+    for label in labels {
+        responses.push(ui.add(label.sense(Sense::click())));
+    }
+
+    let mut response =
+        responses
+            .iter()
+            .fold(responses[0].clone(), |mut overall, current| {
+                overall |= current.clone();
+                overall
+            });
+
+    response |= radio_response;
+
     ui.end_row();
+
+    if response.on_hover_ui_at_pointer(draw_hover).clicked() {
+        *clicked_item = Some((*received).clone());
+    }
 }
