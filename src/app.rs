d5fe115595adcedd9e8b1c3ead0e3f9194917260
Samuel Boyden <sam@stboyden.com>
2022-01-16 00:59:07 +0000

    Remove unnecessary ui.vertical (#15)

--- src/app.rs
+++ src/app.rs
@@ -307,115 +307,113 @@ impl epi::App for App {
             .resizable(false)
             .default_height(200.0)
             .show(ctx, |ui| {
-                ui.vertical(|ui| {
-                    ui.horizontal_wrapped(|ui| {
-                        ui.menu_button("File", |ui| {
-                            if ui.button("Save").clicked() {
-                                self.save_file();
-                            }
-
-                            if ui.button("Load").clicked() {
-                                self.load_file();
-                            }
-
-                            ui.separator();
-
-                            ui.checkbox(&mut self.data.is_autosave, "Auto Save");
-
-                            if ui.button("Settings").clicked() {
-                                self.data.is_settings_open = !self.data.is_settings_open;
-                            }
-
-                            ui.separator();
-
-                            if ui.button("Quit").clicked() {
-                                process::exit(0);
-                            }
-                        });
-
-                        ui.menu_button("Help", |ui| {
-                            if ui.button("About").clicked() {
-                                self.data.is_about_open = !self.data.is_about_open;
-                            }
-                        });
-
-                        ui.separator();
-
-                        ui.label("Filter: ");
-                        ui.text_edit_singleline(&mut self.data.search_filter);
-
-                        // u1f5d9 = 🗙
-                        if ui.button("\u{1f5d9}").clicked() {
-                            self.data.search_filter = "".into();
+                ui.horizontal_wrapped(|ui| {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Save").clicked() {
+                            self.save_file();
                         }
 
-                        ui.label("Filter by:");
-                        egui::ComboBox::from_label("")
-                            .selected_text(format!("{}", self.data.filter_by))
-                            .show_ui(ui, |ui| {
-                                ui.selectable_value(
-                                    &mut self.data.filter_by,
-                                    Filter::Message,
-                                    format!("{}", Filter::Message),
-                                );
-
-                                ui.selectable_value(
-                                    &mut self.data.filter_by,
-                                    Filter::Time,
-                                    format!("{}", Filter::Time),
-                                );
-
-                                ui.selectable_value(
-                                    &mut self.data.filter_by,
-                                    Filter::FileName,
-                                    format!("{}", Filter::FileName),
-                                );
-
-                                ui.selectable_value(
-                                    &mut self.data.filter_by,
-                                    Filter::Address,
-                                    format!("{}", Filter::Address),
-                                );
-
-                                ui.selectable_value(
-                                    &mut self.data.filter_by,
-                                    Filter::LineNumber,
-                                    format!("{}", Filter::LineNumber),
-                                );
-                            });
-
-                        ui.checkbox(&mut self.data.is_case_sensitive, "Case sensitive");
-                        ui.checkbox(&mut self.data.is_using_regex, "Regex");
-                        ui.checkbox(
-                            &mut self.data.do_scroll_to_selected_log,
-                            "Scroll to selected log",
-                        );
-
-                        if ui
-                            .button(
-                                if self.data.is_newest_first {
-                                    "\u{2b07} Newest first" // u2b07 = ⬇
-                                } else {
-                                    "\u{2b06} Newest last" // u2b06 = ⬆
-                                },
-                            )
-                            .clicked()
-                        {
-                            self.data.is_newest_first = !self.data.is_newest_first;
-                        }
-
-                        // u1f5d1 = ��
-                        if ui.button("\u{1f5d1} Clear logs").clicked() {
-                            if let Ok(mut received) = self.data.received.write() {
-                                received.clear();
-                                self.data.clicked_item = None;
-                            }
+                        if ui.button("Load").clicked() {
+                            self.load_file();
                         }
 
                         ui.separator();
 
-                        ui.label(format!("Listening on: {}", self.socket_address));
+                        ui.checkbox(&mut self.data.is_autosave, "Auto Save");
+
+                        if ui.button("Settings").clicked() {
+                            self.data.is_settings_open = !self.data.is_settings_open;
+                        }
+
+                        ui.separator();
+
+                        if ui.button("Quit").clicked() {
+                            process::exit(0);
+                        }
                     });
+
+                    ui.menu_button("Help", |ui| {
+                        if ui.button("About").clicked() {
+                            self.data.is_about_open = !self.data.is_about_open;
+                        }
+                    });
+
+                    ui.separator();
+
+                    ui.label("Filter: ");
+                    ui.text_edit_singleline(&mut self.data.search_filter);
+
+                    // u1f5d9 = 🗙
+                    if ui.button("\u{1f5d9}").clicked() {
+                        self.data.search_filter = "".into();
+                    }
+
+                    ui.label("Filter by:");
+                    egui::ComboBox::from_label("")
+                        .selected_text(format!("{}", self.data.filter_by))
+                        .show_ui(ui, |ui| {
+                            ui.selectable_value(
+                                &mut self.data.filter_by,
+                                Filter::Message,
+                                format!("{}", Filter::Message),
+                            );
+
+                            ui.selectable_value(
+                                &mut self.data.filter_by,
+                                Filter::Time,
+                                format!("{}", Filter::Time),
+                            );
+
+                            ui.selectable_value(
+                                &mut self.data.filter_by,
+                                Filter::FileName,
+                                format!("{}", Filter::FileName),
+                            );
+
+                            ui.selectable_value(
+                                &mut self.data.filter_by,
+                                Filter::Address,
+                                format!("{}", Filter::Address),
+                            );
+
+                            ui.selectable_value(
+                                &mut self.data.filter_by,
+                                Filter::LineNumber,
+                                format!("{}", Filter::LineNumber),
+                            );
+                        });
+
+                    ui.checkbox(&mut self.data.is_case_sensitive, "Case sensitive");
+                    ui.checkbox(&mut self.data.is_using_regex, "Regex");
+                    ui.checkbox(
+                        &mut self.data.do_scroll_to_selected_log,
+                        "Scroll to selected log",
+                    );
+
+                    if ui
+                        .button(
+                            if self.data.is_newest_first {
+                                "\u{2b07} Newest first" // u2b07 = ⬇
+                            } else {
+                                "\u{2b06} Newest last" // u2b06 = ⬆
+                            },
+                        )
+                        .clicked()
+                    {
+                        self.data.is_newest_first = !self.data.is_newest_first;
+                    }
+
+                    // u1f5d1 = ��
+                    if ui.button("\u{1f5d1} Clear logs").clicked() {
+                        if let Ok(mut received) = self.data.received.write() {
+                            received.clear();
+                            self.data.clicked_item = None;
+                        }
+                    }
+
+                    ui.separator();
+
+                    ui.label(format!("Listening on: {}", self.socket_address));
                 });
             });
 
