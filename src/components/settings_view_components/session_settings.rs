67ed2c03c34620be580effe27f0220aa91447578
STBoyden <sam@stboyden.com>
2022-02-06 17:25:21 +0000

    Indent session_settings for better clarity

--- src/components/settings_view_components/session_settings.rs
+++ src/components/settings_view_components/session_settings.rs
@@ -12,34 +12,36 @@ pub fn draw_session_settings(
 
     ui.add_space(10.0);
 
-    ui.label("Alerts:");
+    ui.indent((), |ui| {
+        ui.label("Alerts:");
 
-    ui.indent("alerts", |ui| {
-        if message_alerts.is_empty() {
-            ui.label("None");
-        } else {
-            egui::Grid::new("alert_grid").show(ui, |ui| {
-                for alert in message_alerts.clone() {
-                    ui.label(&alert);
+        ui.indent("alerts", |ui| {
+            if message_alerts.is_empty() {
+                ui.label("None");
+            } else {
+                egui::Grid::new("alert_grid").show(ui, |ui| {
+                    for alert in message_alerts.clone() {
+                        ui.label(&alert);
 
-                    if ui.button("Delete").clicked() {
-                        message_alerts.remove(&alert.clone());
+                        if ui.button("Delete").clicked() {
+                            message_alerts.remove(&alert.clone());
+                        }
+
+                        ui.end_row();
                     }
+                });
+            }
+        });
 
-                    ui.end_row();
-                }
-            });
-        }
-    });
+        ui.horizontal(|ui| {
+            ui.label("New alert:");
+            ui.text_edit_singleline(alert_string);
 
-    ui.horizontal(|ui| {
-        ui.label("New alert:");
-        ui.text_edit_singleline(alert_string);
+            if ui.button("+").clicked() && !alert_string.is_empty() {
+                message_alerts.insert(alert_string.clone());
 
-        if ui.button("+").clicked() && !alert_string.is_empty() {
-            message_alerts.insert(alert_string.clone());
-
-            *alert_string = "".into();
-        }
+                *alert_string = "".into();
+            }
+        });
     });
 }
