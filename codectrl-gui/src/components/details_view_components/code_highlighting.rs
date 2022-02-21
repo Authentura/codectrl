897583cc59a719c04af2ba0c9d1cbcec7a376ae2
Collin Joseph <collinjoseph009@gmail.com>
2022-02-21 23:53:54 +0000

    Fixed line numbering

--- codectrl-gui/src/components/details_view_components/code_highlighting.rs
+++ codectrl-gui/src/components/details_view_components/code_highlighting.rs
@@ -5,15 +5,6 @@ use tree_sitter_highlight::{
     HighlightEvent
 };
 use std::collections::HashMap;
-// Background: (46, 46, 46); #2e2e2e
-// Comments: (121, 121, 121); #797979
-// White: (214, 214, 214); #d6d6d6
-// Yellow: (229, 181, 103); #e5b567
-// Green: (180, 210, 115); #b4d273
-// Orange: (232, 125, 62); #e87d3e
-// Purple: (158, 134, 200); #9e86c8
-// Pink: (176, 82, 121); #b05279
-// Blue: (108, 153, 187); #6c99bb
 
 const BG : egui::Color32 = egui::Color32::from_rgb(46, 46, 46);
 const COMMENTS : egui::Color32 = egui::Color32::from_rgb(121, 121, 121);
@@ -25,17 +16,15 @@ const PURPLE : egui::Color32 = egui::Color32::from_rgb(158, 134, 200);
 const PINK : egui::Color32 = egui::Color32::from_rgb(176, 82, 121);
 const BLUE : egui::Color32 = egui::Color32::from_rgb(120,220,232);
 
-
-pub fn code_highlighter(s: &str) -> egui::text::LayoutJob {
+pub fn code_highlighter(code: &str) -> egui::text::LayoutJob {
 
     // Remove Line number and things
 
-    let mut code : String = "".to_string();
-    for l in s.split('\n'){
-        code.push_str(l[3..].trim());
-        code.push('\n');
-    }
-
+    // let mut code : String = "".to_string();
+    // for l in s.split('\n'){
+    //     code.push_str(l[3..].trim());
+    //     code.push('\n');
+    // }
     let highlight_data = HashMap::from([
         ("keyword",ORANGE),
         ("operator",ORANGE),
@@ -98,6 +87,7 @@ pub fn code_highlighter(s: &str) -> egui::text::LayoutJob {
 
     let mut job = egui::text::LayoutJob::default();
     let mut current = egui::Color32::TEMPORARY_COLOR;
+    let mut line_number : u8 = 0;
 
     for event in highlights {
         match event.unwrap() {
