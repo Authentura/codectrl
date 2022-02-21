5f68e469cf85b068970fffef7bc431a0cbdabf7b
Collin Joseph <collinjoseph009@gmail.com>
2022-02-21 23:24:41 +0000

    Setup code highlighting for Python

--- codectrl-gui/src/components/details_view_components/code_highlighting.rs
+++ codectrl-gui/src/components/details_view_components/code_highlighting.rs
@@ -1,10 +1,10 @@
-use egui::{CtxRef, RichText, TextStyle, Ui, TextFormat};
+use egui::{TextFormat};
 use tree_sitter_highlight::{
     Highlighter,
     HighlightConfiguration,
     HighlightEvent
 };
-
+use std::collections::HashMap;
 // Background: (46, 46, 46); #2e2e2e
 // Comments: (121, 121, 121); #797979
 // White: (214, 214, 214); #d6d6d6
@@ -15,15 +15,15 @@ use tree_sitter_highlight::{
 // Pink: (176, 82, 121); #b05279
 // Blue: (108, 153, 187); #6c99bb
 
-const bg = egui::Color32::from_rgb(46, 46, 46);
-const comments = egui::Color32::from_rgb(121, 121, 121);
-const white = egui::Color32::from_rgb(214, 214, 214);
-const yellow = egui::Color32::from_rgb(180, 210, 115);
-const green = egui::Color32::from_rgb(229, 181, 103);
-const orange = egui::Color32::from_rgb(232, 125, 62);
-const purple = egui::Color32::from_rgb(158, 134, 200);
-const pink = egui::Color32::from_rgb(176, 82, 121);
-const blue = egui::Color32::from_rgb(108, 153, 187);
+const BG : egui::Color32 = egui::Color32::from_rgb(46, 46, 46);
+const COMMENTS : egui::Color32 = egui::Color32::from_rgb(121, 121, 121);
+const WHITE : egui::Color32 = egui::Color32::from_rgb(214, 214, 214);
+const YELLOW : egui::Color32 = egui::Color32::from_rgb(255,216,102);
+const GREEN : egui::Color32 = egui::Color32::from_rgb(169,220,118);
+const ORANGE : egui::Color32 = egui::Color32::from_rgb(255,97,136);
+const PURPLE : egui::Color32 = egui::Color32::from_rgb(158, 134, 200);
+const PINK : egui::Color32 = egui::Color32::from_rgb(176, 82, 121);
+const BLUE : egui::Color32 = egui::Color32::from_rgb(120,220,232);
 
 
 pub fn code_highlighter(s: &str) -> egui::text::LayoutJob {
@@ -36,28 +36,46 @@ pub fn code_highlighter(s: &str) -> egui::text::LayoutJob {
         code.push('\n');
     }
 
+    let highlight_data = HashMap::from([
+        ("keyword",ORANGE),
+        ("operator",ORANGE),
+        ("escape",ORANGE),
 
-    let highlight_names = &[
-        "keyword",
-        "function",
-        "variable",
-        "operator",
-        "attribute",
-        "constant",
-        "property",
-        "punctuation",
-        "punctuation.bracket",
-        "punctuation.delimiter",
-        "string",
-        "string.special",
-        "tag",
-        "number",
-        "type",
-        "function.builtin",
-        "type.builtin",
-        "variable.builtin",
-        "variable.parameter",
-    ];
+        ("function.builtin",BLUE),
+        ("function",BLUE),
+        ("constant",PURPLE),
+
+        ("variable",WHITE),
+        ("number",PURPLE),
+        ("string",YELLOW),
+
+        ("comment", COMMENTS),
+    ]);
+    
+    let highlight_names = Vec::from_iter(highlight_data.keys());
+    // let highlight_names = &[
+    //     "keyword",
+    //     "function",
+    //     "variable",
+    //     "operator",
+    //     "attribute",
+    //     "constant",
+    //     "property",
+    //     "punctuation",
+    //     "punctuation.bracket",
+    //     "punctuation.delimiter",
+    //     "string",
+    //     "string.special",
+    //     "tag",
+    //     "number",
+    //     "type",
+    //     "function.builtin",
+    //     "type.builtin",
+    //     "variable.builtin",
+    //     "variable.parameter",
+    // ];
+    
+    // code = std::fs::read_to_string("codectrl-gui/src/components/details_view_components/syntax-check.py").expect("it doesnt work");
 
     let mut javascript_config = HighlightConfiguration::new(
         tree_sitter_python::language(),
@@ -66,8 +84,7 @@ pub fn code_highlighter(s: &str) -> egui::text::LayoutJob {
         ""
     ).unwrap();
 
-    javascript_config.configure(highlight_names);
-
+    javascript_config.configure(&highlight_names);
 
     let mut highlighter = Highlighter::new();
 
@@ -78,38 +95,27 @@ pub fn code_highlighter(s: &str) -> egui::text::LayoutJob {
         |_| None
     ).unwrap();
     
-    let color = [
-        egui::Color32::from_rgb(248, 85, 82),
-        egui::Color32::from_rgb(141, 161, 1),
-        egui::Color32::from_rgb(230, 152, 117),
-        egui::Color32::from_rgb(230, 152, 117),
-        egui::Color32::from_rgb(141, 161, 1),
-        egui::Color32::from_rgb(122, 132, 120),
-    ];
 
     let mut job = egui::text::LayoutJob::default();
-    let mut current = 0;
+    let mut current = egui::Color32::TEMPORARY_COLOR;
 
     for event in highlights {
         match event.unwrap() {
             HighlightEvent::Source {start, end} => {
-                eprintln!("source: {}-{}", start, end);
+                eprintln!("source: {}", &code[start..end]);
                 job.append(
                     &code[start..end],
                     0.0,
-                    TextFormat::simple(egui::TextStyle::Monospace,color[current])
-                )
+                    TextFormat::simple(egui::TextStyle::Monospace,current)
+                );
             },
             HighlightEvent::HighlightStart(s) => {
                 eprintln!("highlight style started: {:?}", highlight_names[s.0]);
-                if s.0 < 4{
-                    current = s.0
-                }
-
+                current = highlight_data[highlight_names[s.0]];
             },
             HighlightEvent::HighlightEnd => {
                 eprintln!("highlight style ended");
-                current = 5
+                current = WHITE;
             },
         }
     }
