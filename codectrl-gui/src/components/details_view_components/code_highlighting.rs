2101b279888ac58ec1dc76da30b16740901bd43e
Collin Joseph <collinjoseph009@gmail.com>
2022-03-01 21:24:49 +0000

    formatting

--- codectrl-gui/src/components/details_view_components/code_highlighting.rs
+++ codectrl-gui/src/components/details_view_components/code_highlighting.rs
@@ -1,33 +1,35 @@
 use egui::TextFormat;
-use syntect::easy::HighlightLines;
-use syntect::parsing::SyntaxSet;
-use syntect::highlighting::{ThemeSet, Style};
-use syntect::util::{ LinesWithEndings};
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::LinesWithEndings,
+};
 
-pub fn code_highlighter(code: &str, lang: &str) -> egui::text::LayoutJob{
-
+pub fn code_highlighter(code: &str, lang: &str) -> egui::text::LayoutJob {
     let ps = SyntaxSet::load_defaults_newlines();
     let ts = ThemeSet::load_defaults();
 
     let syntax = ps.find_syntax_by_extension("py").unwrap();
     let mut h = HighlightLines::new(syntax, &ts.themes["base16-mocha.dark"]);
-    println!("{:?}",&ts.themes.keys());
     let mut job = egui::text::LayoutJob::default();
     for line in LinesWithEndings::from(code) {
         let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
-        for h in ranges{
+        for h in ranges {
             let (style, code) = h;
             job.append(
                 code,
                 0.0,
-                TextFormat::simple(egui::TextStyle::Monospace, egui::Color32::from_rgb(
-                    style.foreground.r,
-                    style.foreground.g,
-                    style.foreground.b
-                )),
+                TextFormat::simple(
+                    egui::TextStyle::Monospace,
+                    egui::Color32::from_rgb(
+                        style.foreground.r,
+                        style.foreground.g,
+                        style.foreground.b,
+                    ),
+                ),
             );
         }
-
     }
     job
 }
