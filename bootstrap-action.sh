f26a0a8db62f1b9ba6d8e8ac816cda2ed588ab19
Samuel Boyden <sam@stboyden.com>
2022-07-11 16:37:08 +0100

    base-devel -> build-essential: should fix action error

--- bootstrap-action.sh
+++ bootstrap-action.sh
@@ -9,7 +9,7 @@ echo -e "\nInstalling dependencies for your distro...\n"
 
 case $ID in
     "debian" | "ubuntu" | "elementary")
-        sudo apt install base-devel gcc clang -y
+        sudo apt install build-essential gcc clang -y
         sudo apt install libglib2.0-dev libpango1.0-dev libgdk-pixbuf-2.0-dev libatk1.0-dev libgtk-3-dev libxcb-shape0-dev libxcb-xfixes0-dev -y
     ;;
     "fedora")
