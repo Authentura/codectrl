b5942c69e0d3ea1809e2b5e9fe64155ce66a1866
Samuel Boyden <sam@stboyden.com>
2022-07-11 16:51:46 +0100

    Use 22.04, install curl

--- bootstrap-action.sh
+++ bootstrap-action.sh
@@ -10,11 +10,11 @@ echo -e "\nInstalling dependencies for your distro...\n"
 case $ID in
     "debian" | "ubuntu" | "elementary")
         sudo apt install build-essential gcc clang -y
-        sudo apt install libglib2.0-dev libpango1.0-dev libgdk-pixbuf-2.0-dev libatk1.0-dev libgtk-3-dev libxcb-shape0-dev libxcb-xfixes0-dev -y
+        sudo apt install libglib2.0-dev libpango1.0-dev libgdk-pixbuf-2.0-dev libatk1.0-dev libgtk-3-dev libxcb-shape0-dev libxcb-xfixes0-dev curl -y
     ;;
     "fedora")
         sudo dnf groupinstall "Development Tools" -y
-        sudo dnf install gobject-introspection-devel cairo-devel atk-devel pango-devel gdk-pixbuf2-devel gtk3-devel clang -y
+        sudo dnf install gobject-introspection-devel cairo-devel atk-devel pango-devel gdk-pixbuf2-devel gtk3-devel clang curl -y
     ;;
 esac
 
