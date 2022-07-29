8445103f3da8f055da7d3abd946373b6a920df50
Samuel Boyden <sam@stboyden.com>
2022-07-29 18:20:32 +0100

    Move boostrap back into Dockerfiles

--- .github/actions/run-fedora-rawhide/entrypoint.sh
+++ .github/actions/run-fedora-rawhide/entrypoint.sh
@@ -1,13 +1,6 @@
-#!/bin/sh -l
+#!/bin/bash
 
-set -xe
-
-/bootstrap-action.sh
-
-echo "Installing rustup..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- --default-toolchain nightly -y
 source $HOME/.cargo/env
 
-cargo install cargo-generate-rpm
-
-$1
+echo -e "\nExecuting \"$@\""
+$@
