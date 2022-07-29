6b513c601d199d0bcdcf7401a8150fb81e8da26d
Samuel Boyden <sam@stboyden.com>
2022-07-29 16:55:47 +0100

    Avoid installing rustup until entrypoint

--- .github/actions/run-fedora-latest/entrypoint.sh
+++ .github/actions/run-fedora-latest/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -l
 
-. $HOME/.cargo/env
+set -xe
+
+/bootstrap-action.sh
+
+echo "Installing rustup..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- --default-toolchain nightly -y
+source $HOME/.cargo/env
+
+cargo install cargo-generate-rpm
 
 $1
