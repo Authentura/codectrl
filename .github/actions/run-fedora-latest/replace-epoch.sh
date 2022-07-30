2d086835156ee95b1c92e8c431eda184a5c69cfe
Samuel Boyden <sam@stboyden.com>
2022-07-30 01:11:00 +0100

    Manually replace Cargo.toml with mv

--- .github/actions/run-fedora-latest/replace-epoch.sh
+++ .github/actions/run-fedora-latest/replace-epoch.sh
@@ -2,4 +2,6 @@
 
 set -xe
 
-sed "s/<>/$1/g" "$2/Cargo.toml" > "$2/Cargo.toml"
+sed "s/<>/$1/g" "$2/Cargo.toml" > "$2/Cargo.toml.new"
+mv "$2/Cargo.toml" "$2/Cargo.toml.old"
+mv "$2/Cargo.toml.new" "$2/Cargo.toml"
