77f6775596cc9c4d4aaa1f7d84b390d2f5e579bf
Samuel Boyden <sam@stboyden.com>
2022-08-09 19:11:31 +0100

    Use nix-shell to build and cache result before packaging

--- shell.nix
+++ shell.nix
@@ -1,12 +1,27 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  mozillaOverlay ? import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz),
+  pkgs ? import <nixpkgs> {
+      overlays = [ mozillaOverlay ];
+  },
+  rust ? (pkgs.rustChannelOf { channel = "stable"; }).rust,
+  rustPlatform ? pkgs.makeRustPlatform {
+      cargo = rust;
+      rustc = rust;
+  },
+}:
+
+
 pkgs.mkShell {
-  name = "dev";
+  name = "build-shell";
   buildInputs = [
-    pkgs.rustup
+    rustPlatform.rust.cargo
+    rustPlatform.rust.rustc
     pkgs.cmake
+    pkgs.gtk3
   ];
 
   shellHook = ''
-    rustup update
+    export RUSTFLAGS="-Ctarget-feature=+crt-static"
+    echo "Entered Nix-Shell environment..."
   '';
 }
