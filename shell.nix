d60baa897560ac209f671e5d5292fb823293a0eb
Samuel Boyden <sam@stboyden.com>
2022-08-09 22:54:47 +0100

    Don't use Nix

--- shell.nix
+++ shell.nix
@@ -17,33 +17,12 @@ pkgs.stdenv.mkDerivation rec {
   name = "build-shell";
 
   buildInputs = with pkgs; [
-    # clang
-    # mold
-    atk
-    cairo
     cmake
-    gcc
-    gdk-pixbuf
-    glib
-    glibc
-    glibc.static
-    gobject-introspection
     gtk3
-    libxkbcommon
-    pango
     pkg-config
     rustPlatform.rust.cargo
     rustPlatform.rust.rustc
-    wayland
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXi
-    xorg.libXrandr
   ];
-
-  LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
-  # RUSTFLAGS = "-Ctarget-feature=+crt-static -Clinker=clang -Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
-  RUSTFLAGS = "-Ctarget-feature=+crt-static";
 
   shellHook = ''
     echo "Entered Nix-Shell environment..."
