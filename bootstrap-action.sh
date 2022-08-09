fd323747c4bd00dc0b85f10a09e5583873471ef1
Samuel Boyden <sam@stboyden.com>
2022-08-09 23:19:09 +0100

    Install glibc-static during bootstrap

--- bootstrap-action.sh
+++ bootstrap-action.sh
@@ -19,7 +19,7 @@ case $ID in
                 pixbuf_dev="libgdk-pixbuf-2.0-dev"
             ;;
         esac
-        sudo apt install build-essential gcc clang libglib2.0-dev libpango1.0-dev "$pixbuf_dev" libatk1.0-dev libgtk-3-dev libxcb-shape0-dev libxcb-xfixes0-dev curl -y
+        sudo apt install build-essential gcc clang libglib2.0-dev libpango1.0-dev "$pixbuf_dev" libatk1.0-dev libgtk-3-dev libxcb-shape0-dev libxcb-xfixes0-dev curl glibc-static -y
     ;;
     "fedora")
         sudo dnf update -y
