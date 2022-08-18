03ca0a5f74f6151f4f77d34efb21e7b08a2fe3cb
Samuel Boyden <sam@stboyden.com>
2022-08-18 01:47:17 +0100

    Install git as a build-time dependency

--- bootstrap-action.sh
+++ bootstrap-action.sh
@@ -22,12 +22,12 @@ case $ID in
                 pixbuf_dev="libgdk-pixbuf-2.0-dev"
             ;;
         esac
-        apt install build-essential clang cmake libglib2.0-dev libpango1.0-dev "$pixbuf_dev" libatk1.0-dev libgtk-3-dev libxcb-shape0-dev libxcb-xfixes0-dev curl libc6-dev libsqlite3-dev -y
+        apt install build-essential clang cmake libglib2.0-dev libpango1.0-dev "$pixbuf_dev" libatk1.0-dev libgtk-3-dev libxcb-shape0-dev libxcb-xfixes0-dev curl libc6-dev libsqlite3-dev git -y
     ;;
     "fedora")
         sudo dnf update -y
         sudo dnf groupinstall "Development Tools" -y
-        sudo dnf install gobject-introspection-devel cairo-devel atk-devel pango-devel gdk-pixbuf2-devel gtk3-devel clang curl cmake -y
+        sudo dnf install gobject-introspection-devel cairo-devel atk-devel pango-devel gdk-pixbuf2-devel gtk3-devel clang curl cmake git -y
     ;;
 esac
 
