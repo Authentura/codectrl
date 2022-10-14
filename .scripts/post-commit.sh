02fcd3ddae462f363e532495e35c087ea5cee6f5
Samuel Boyden <sam@stboyden.com>
2022-10-14 16:51:42 +0100

    Fix pre-commit and post-commit scripts

--- .scripts/post-commit.sh
+++ .scripts/post-commit.sh
@@ -3,10 +3,13 @@
 if [[ -f .rustfmt-triggered ]]; then
   rm .rustfmt-triggered
   export GIT_SHA="$(git rev-parse HEAD)"
-  echo "# $(git rev-list --format=%ae --max-count=1 $GIT_SHA | tail -n1): $(git rev-list --format=%B --max-count=1 $GIT_SHA | tail -n1)" >> .git-blame-ignore-revs
+  echo "# ($(date -Ihours)) $(git log --format=%ae -1 $GIT_SHA): $(git log --format=%B -1 $GIT_SHA)" >> .git-blame-ignore-revs
   echo "${GIT_SHA}" >> .git-blame-ignore-revs
   echo "" >> .git-blame-ignore-revs
 
   git add .git-blame-ignore-revs
-  git commit --amend --no-edit
+
+  git commit -m "post-commit: add $(git rev-parse --short HEAD) to .git-blame-ignore-revs" \
+             -m "Reason: reformatted code" \
+             --no-verify
 fi
