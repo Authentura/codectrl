40491d0dbb55ff7e3ea9bb2429011ccd92538088
Samuel Boyden <sam@stboyden.com>
2022-10-14 17:43:24 +0100

    Add install script for commit hooks

---
+++ install-commit-hooks.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if ! command -v pre-commit >/dev/null; then
+  echo "Please install pre-commit: pip install pre-commit"
+  exit 1;
+fi
+
+pre-commit install
+pre-commit install --hook-type post-commit
